e66ea1bf71b82f440cdb19aa1682b10cfbf03c37
serafindanyil <danyil.serafin.ir.2023@lpnu.ua>
2024-11-27 12:58:53 +0200

    Add some code

--- main.py
+++ main.py
@@ -1,139 +1,116 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 
-# Параметри схеми для 21 варіанта
-C1 = 1.5e-3  # Фарад
-C2 = 2.9e-3  # Фарад
-L_max = 15  # Генрі
-L_min = 1.5  # Генрі
-i_min = 1  # Ампер
-i_max = 2  # Ампер
-R1 = 13  # Ом
-R2 = 73  # Ом
-R3 = 22  # Ом
-R4 = 31  # Ом
-a = 0.001  # секунд
-T = 6 * a  # Період сигналу
+C1 = 1.5e-3
+C2 = 2.9e-3
+L_max = 15
+L_min = 1.5
+i_min = 1
+i_max = 2
+R1 = 13
+R2 = 73
+R3 = 22
+R4 = 31
+a = 0.001
+T = 6 * a
+h = T / 400
 
-# Чисельні параметри
-h = T / 400  # Крок інтегрування
-steps_per_period = 100  # Точки на період
+x_points = []
+y_points_u2 = []
+y_points_u1 = []
+x1_points = []
+y1_points = []
 
-# Функція напруги живлення U1(t)
-def U1(t):
-    t_mod = t % T
-    if t_mod <= T / 2:
-        return 20 * t_mod / (T / 2) - 10
-    else:
-        return -20 * (t_mod - T / 2) / (T / 2) + 10
 
-# Апроксимація індуктивності L2(i)
-def L2(i2):
-    if abs(i2) <= i_min:
+def L2(i):
+    if abs(i) <= i_min:
         return L_max
-    elif abs(i2) >= i_max:
-        return L_min
+    elif abs(i) < i_max:
+        normalized_i = (abs(i) - i_min) / (i_max - i_min)
+        return L_max - (L_max - L_min) * (3 * normalized_i ** 2 - 2 * normalized_i ** 3)
     else:
-        # Кубічний поліном для апроксимації
-        a0, a1, a2, a3 = L_max, 0, (L_min - L_max) / ((i_max - i_min) ** 2), 0
-        return a0 + a1 * abs(i2) + a2 * abs(i2)**2 + a3 * abs(i2)**3
+        return L_min
 
-# Система диференціальних рівнянь
-def equations(t, y):
-    uC1, uC2, i2 = y
-    duC1_dt = (i2 - uC1 / R1) / C1
-    duC2_dt = i2 / C2
-    di2_dt = (U1(t) - uC1 - uC2 - i2 * (R2 + R4)) / L2(i2)
-    return np.array([duC1_dt, duC2_dt, di2_dt])
 
-# Метод Рунге-Кутта 3-го порядку
-def runge_kutta_3_step(t, y):
-    K1 = h * equations(t, y)
-    K2 = h * equations(t + h / 2, y + K1 / 2)
-    K3 = h * equations(t + h, y - K1 + 2 * K2)
-    return y + (K1 + 4 * K2 + K3) / 6
+def U1(t):
+    phase = t % T
+    if phase <= a:
+        return 10 * phase / a
+    elif phase <= 2 * a:
+        return 10
+    elif phase <= 3 * a:
+        return 10 * (3 * a - phase) / a
+    elif phase <= 4 * a:
+        return -10 * (phase - 3 * a) / a
+    elif phase <= 5 * a:
+        return -10
+    elif phase <= 6 * a:
+        return -10 * (6 * a - phase) / a
+    else:
+        return 0
 
-# Симуляція системи
-def simulate():
+
+def system(t, x):
+    uC1, uC2, iL2 = x
+    duC1_dt = (iL2 - uC1 / R1) / C1
+    duC2_dt = iL2 / C2
+    diL2_dt = (U1(t) - uC1 - uC2 - iL2 * (R2 + R4)) / L2(iL2)
+    return np.array([duC1_dt, duC2_dt, diL2_dt])
+
+
+def runge_kutta_step(t, x):
+    K1 = h * system(t, x)
+    K2 = h * system(t + h / 3, x + K1 / 3)
+    K3 = h * system(t + 2 * h / 3, x + 2 * K2 / 3)
+    return x + (K1 + 3 * K3) / 4
+
+
+def complex_lab():
     t = 0
-    y = np.array([0, 0, 0])  # Початкові умови: uC1, uC2, i2
-    results = []
-
-    for _ in range(5 * steps_per_period):
-        uC1, uC2, i2 = y
-        u2 = uC2 + i2 * R4
-        u1 = U1(t)
-        results.append([t, uC1, uC2, i2, u2, u1])
-        y = runge_kutta_3_step(t, y)
+    x = np.array([0, 0, 0])
+    while t <= 5 * T:
+        uC1, uC2, iL2 = x
+        U2 = uC2 + iL2 * R4
+        x_points.append(t)
+        y_points_u2.append(U2)
+        y_points_u1.append(U1(t))
+        x = runge_kutta_step(t, x)
         t += h
 
-    return np.array(results)
 
-# Виконання симуляції
-results = simulate()
+def check_l():
+    i_vals = np.linspace(0, 3, 300)
+    for i in i_vals:
+        x1_points.append(i)
+        y1_points.append(L2(i))
 
-# Виділення результатів для графіків
-t_values = results[:, 0]
-uC1_values = results[:, 1]
-uC2_values = results[:, 2]
-i2_values = results[:, 3]
-u2_values = results[:, 4]
-u1_values = results[:, 5]
 
-# Побудова графіків
+complex_lab()
+check_l()
+
 plt.figure(figsize=(10, 6))
-plt.plot(t_values, uC1_values, label="UC1(t)", color='b')
-plt.title("UC1 / t")
-plt.xlabel("Час (с)")
-plt.ylabel("Напруга (В)")
+plt.plot(x_points, y_points_u1, color='orange', label="U1(t)")
+plt.title("Вхідна напруга U1(t)")
+plt.xlabel("Час, с")
+plt.ylabel("Напруга, В")
 plt.grid(True)
 plt.legend()
 plt.show()
 
 plt.figure(figsize=(10, 6))
-plt.plot(t_values, uC2_values, label="UC2(t)", color='m')
-plt.title("UC2 / t")
-plt.xlabel("Час (с)")
-plt.ylabel("Напруга (В)")
+plt.plot(x_points, y_points_u2, color='g', label="U2(t)")
+plt.title("Вихідна напруга U2(t)")
+plt.xlabel("Час, с")
+plt.ylabel("Напруга, В")
 plt.grid(True)
 plt.legend()
 plt.show()
 
 plt.figure(figsize=(10, 6))
-plt.plot(t_values, i2_values, label="I2(t)", color='c')
-plt.title("I2 / t")
-plt.xlabel("Час (с)")
-plt.ylabel("Струм (А)")
-plt.grid(True)
-plt.legend()
-plt.show()
-
-plt.figure(figsize=(10, 6))
-plt.plot(t_values, u2_values, label="U2(t)", color='g')
-plt.title("U2 / t")
-plt.xlabel("Час (с)")
-plt.ylabel("Напруга (В)")
-plt.grid(True)
-plt.legend()
-plt.show()
-
-plt.figure(figsize=(10, 6))
-plt.plot(t_values, u1_values, label="U1(t)", color='orange')
-plt.title("U1 / t")
-plt.xlabel("Час (с)")
-plt.ylabel("Напруга (В)")
-plt.grid(True)
-plt.legend()
-plt.show()
-
-# Побудова графіка L2 / i
-i_vals = np.linspace(-3, 3, 300)
-l_vals = [L2(i) for i in i_vals]
-plt.figure(figsize=(10, 6))
-plt.plot(i_vals, l_vals, label="L2(I2)", color='r')
-plt.title("L2 / I2")
-plt.xlabel("Струм I2 (А)")
-plt.ylabel("Індуктивність L2 (Гн)")
+plt.plot(x1_points, y1_points, color='r', label="L2(i)")
+plt.title("Індуктивність L2(i)")
+plt.xlabel("Струм, А")
+plt.ylabel("Індуктивність, Гн")
 plt.grid(True)
 plt.legend()
 plt.show()
